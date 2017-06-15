5df136580093f8b8806a14017d984e5b27f29af9
Mauro <Mauro@DESKTOP-CIPCFTU>
2017-06-15 16:10:01 +0100

    Add project files.

---
+++ Cap06_Jogo_Damas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap06_Jogo_Damas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
